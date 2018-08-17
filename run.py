7e2522f4100f93d127f4f91ee4caa37f3158fdf7
Colin Fallon <c.fallon@cern.ch>
2018-08-17 14:19:32 -0500

    Fixed bugs preventing proper symlink removal

--- run.py
+++ run.py
@@ -7,11 +7,14 @@ if not len(argv)==5:
 	print "2-input type Usage:\n python run.py macros/analysisClass_uHBHECommissioning.C config/input_lists/main_input.txt config/tree_lists/tree_list.txt output.root"
 	exit(1)
 else:
-	if(os.path.exists("./src/analysisClass.C")):
-		if not argv[1] in  os.readlink("./src/analysisClass.C") and os.path.exists("./src/analysisClass.o"):
-			os.remove("./src/analysisClass.o")
-		os.remove("./src/analysisClass.C")
-	os.symlink("../%s"%argv[1],"./src/analysisClass.C")
+	if os.path.lexists("src/analysisClass.C"):
+		if os.path.lexists("src/analysisClass.o"):
+			os.remove("src/analysisClass.o")
+			print("Removed old analysisClass.o")
+		os.remove("src/analysisClass.C")
+		print("Removed old analysisClass.C")
+	print("Making new symlink analysisClass.C")
+	os.symlink("../%s"%argv[1],"src/analysisClass.C")
 	exitcode = call("make")
 	if not exitcode==0:
 		print "Compile failed ... "
